06fcc9dbb1159adab7d1dfb3230d0e6d560ea294
alexandre i w <alexandreinaciowatanabe@gmail.com>
2023-10-17 17:08:55 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Home page</h1>
+    <main className="flex h-screen w-full items-center justify-center bg-gray-900">
+      <h1 className="text-violet-300">Home page</h1>
     </main>
   );
 }
